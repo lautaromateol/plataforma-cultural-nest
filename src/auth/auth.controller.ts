c17b9dc174348798f951b4a87171f72aa445cb04
lautaromateol <lautaromateol@gmail.com>
2025-12-17 14:18:30 -0300

    fix: cookies mana

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,14 @@
 import {
   Body,
   Controller,
-  Delete,
   HttpCode,
   HttpStatus,
   Patch,
   Post,
   Query,
-  Res,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateEmailSchema, LoginSchema, ResendEmailSchema } from './schemas';
-import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -19,24 +16,8 @@ export class AuthController {
 
   @Post('login')
   @HttpCode(HttpStatus.CREATED)
-  async login(
-    @Body() userData: LoginSchema,
-    @Res({ passthrough: true }) response: Response,
-  ) {
-    const { access_token, role } = await this.authService.login(userData);
-
-    response.cookie('access_token', access_token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
-      maxAge: 24 * 60 * 60 * 1000,
-      path: '/',
-    });
-
-    return {
-      ok: true,
-      role,
-    };
+  async login(@Body() userData: LoginSchema) {
+    return await this.authService.login(userData);
   }
 
   @Patch('create-email')
@@ -55,13 +36,5 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   async resendEmail(@Body() userData: ResendEmailSchema) {
     return await this.authService.resendEmail(userData);
-  }
-
-  @Delete('logout')
-  @HttpCode(HttpStatus.OK)
-  async logout(@Res({ passthrough: true }) response: Response) {
-    response.clearCookie('access_token');
-
-    return { ok: true };
   }
 }
