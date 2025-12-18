67b5d76f00da38f7818a9db541e8fb69a22b0d2e
lautaromateol <lautaromateol@gmail.com>
2025-12-18 17:46:23 -0300

    update: prisma module and course controller and services added to course module

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { CourseService } from './course.service';
+import { CourseController } from './course.controller';
 
-@Module({})
+@Module({
+  imports: [PrismaModule],
+  providers: [CourseService],
+  controllers: [CourseController],
+})
 export class CourseModule {}
